aed0dc2185178d677e1648649a7b2373199686ce
jasmaa <jasonmaa3955@gmail.com>
2017-11-28 21:26:22 -0500

    Add scene transition

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	Image fadeScreen;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (gameObject);
+		}
+
+		fadeScreen = GameObject.Find ("FadeScreen").GetComponent<Image>();
+	}
+	
+	public void GoNextScene(){
+		FadeFromBlack ();
+		SceneManager.LoadScene ((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount + 1);
+	}
+	
+	public void FadeToBlack(){
+			fadeScreen.color = Color.black;
+			fadeScreen.canvasRenderer.SetAlpha (0.0f);
+			fadeScreen.CrossFadeAlpha(1.0f, 0.5f, false);
+	}
+	public void FadeFromBlack(){
+		fadeScreen.color = Color.black;
+		fadeScreen.canvasRenderer.SetAlpha (1.0f);
+		fadeScreen.CrossFadeAlpha(0.0f, 0.5f, false);
+	}
+}
---
+++ Assets/_Project/Scripts/GoOnBut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoOnBut : MonoBehaviour {
+
+	public Material highlight;
+	public Material norm;
+
+	public void GoOn(){
+		GameManager.instance.GoNextScene ();
+	}
+
+	public void Highlight(){
+		GetComponent<Renderer> ().material = highlight;
+	}
+	public void UnHighlight(){
+		GetComponent<Renderer> ().material = norm;
+	}
+}
