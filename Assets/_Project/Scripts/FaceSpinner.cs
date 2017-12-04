043250b7c80318960d0810eda0475ae80f57ad57
jasmaa <jasonmaa3955@gmail.com>
2017-12-04 08:08:59 -0500

    Move globe up, limit face rotation

---
+++ Assets/_Project/Scripts/FaceSpinner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceSpinner : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler (0, 90*Mathf.Sin (Time.time), 0);
+	}
+}
